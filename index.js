e59fca17f4f2b29a9a71b126a61c546d3f81f6d7
Guilherme Eiras <guialbertoeiras@hotmail.com>
2020-05-01 12:39:25 -0300

    feat(room): add create and join room

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+const randomstring = require('randomstring');
 const rooms = {};
 
 app.use(cors());
@@ -10,10 +11,37 @@ app.get('/', (req, res) => {
   res.send('<h1>Spyfall</h1>');
 });
 
+function roomLobby(room) {
+  return {
+    id: room.id,
+    players: room.players.map((socket) => socket.player)
+  }
+}
+
 io.on('connection', (socket) => {
   console.log('connected ' + socket.id);
   socket.on('disconnect', () => {
     console.log('disconnected ' + socket.id);
+  });
+  socket.on('new room', ({ player }) => {
+    socket.player = player;
+    const roomId = randomstring.generate({ length: 6, capitalization: 'uppercase' });
+    const room = { id: roomId, state: 'lobby', players: [socket] };
+    rooms[roomId] = room;
+    socket.room = room;
+    socket.emit('lobby', roomLobby(room));
+  });
+
+  socket.on('join room', ({ player, room: roomId }) => {
+    socket.player = player;
+    const room = rooms[roomId];
+    if (room && room.state === 'lobby') {
+      room.players.push(socket);
+      socket.room = room;
+      room.players.forEach(socket => {
+        socket.emit('lobby', roomLobby(room));
+      });
+    }
   });
 });
 
