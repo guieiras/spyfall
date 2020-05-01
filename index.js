3b298faf2117110738eddd0a6d216d36cf1b6cbd
Guilherme Eiras <guialbertoeiras@hotmail.com>
2020-05-01 11:16:51 -0300

    feat(server): add cors

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-var app = require('express')();
-var http = require('http').createServer(app);
+const app = require('express')();
+const cors = require('cors');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
+const rooms = {};
+
+app.use(cors());
 app.get('/', (req, res) => {
   res.send('<h1>Spyfall</h1>');
 });
 
-http.listen(3000, () => {
-  console.log('Listening on *:3000');
+io.on('connection', (socket) => {
+  console.log('connected ' + socket.id);
+  socket.on('disconnect', () => {
+    console.log('disconnected ' + socket.id);
+  });
+});
+
+http.listen(3001, () => {
+  console.log('Listening on *:3001');
 });
